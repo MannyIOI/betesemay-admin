303b5b71b2a57974c991722b5af214ebf5a12998
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-02-26 14:38:34 +0300

    navbar basics added

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    min-width: 1500px;
+    width: 300px;
+    height: 100px;
+    display: flex;
+    align-items: center;
 `
 
 export const NavLinks = styled.div`
     .nav-item {
         opacity: 0.6;
-        color: 'white';
+        color: white;
     }
     .active-nav-item {
         opacity: 1;
@@ -18,6 +21,11 @@ export const NavLinks = styled.div`
     .hover-active:hover {
         opacity: 1;
     }
+
+    background: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `
 
 export const Inner = styled.div`
---
+++ src/screens/ItemsScreen/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const Container = styeld.div`
+    min-width: 1500px;
+    display: flex;
+`
