e243bc39e2b56751f6b30d66e9963d8b43c845aa
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-04 03:12:16 +0300

    feat(ItemHistory) createI tem History

---
+++ src/screens/ItemHistoryScreen/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Container } from "./style";
+
+export const ItemHistory = () => {
+    return (
+        <Container>
+            Create Employee Screen
+        </Container>
+    )
+}
