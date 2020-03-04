a26514af06a21820e7831b28d79abc07d0e4dbbb
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-04 03:12:40 +0300

    feat (LendReturn) create Screen

---
+++ src/screens/LendReturnScreen/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Container } from "./style";
+
+export const LendReturn = () => {
+    return (
+        <Container>
+            Lend Return Screen
+        </Container>
+    )
+}
