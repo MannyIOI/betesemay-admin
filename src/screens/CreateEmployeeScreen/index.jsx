8509c235aba66fd7fd7e0407be9aa8448b118b4b
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-04 06:57:55 +0300

    fix (CreateEmployee) create hooks

--- src/screens/CreateEmployeeScreen/index.jsx
+++ src/screens/CreateEmployeeScreen/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Container } from "./style";
+import { Container, Input, SubmitBtn } from "./style";
 
 const CreateEmployee = () => {
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [jobTitle, setJobTitle] = useState("")
+
+    
+
     return (
         <Container>
             Create Employee Screen
+            {/* "phoneNumber", "firstName", "lastName", "jobTitle" */}
+            <Input placeholder="Email"/>
+            <Input placeholder="Phone Number" />
+            <Input placeholder="Last Name" />
+            <Input placeholder="Job Title" />
+            <SubmitBtn>Submit</SubmitBtn>
         </Container>
     )
 }
--- src/screens/CreateEmployeeScreen/style.js
+++ src/screens/CreateEmployeeScreen/style.js
@@ -3,3 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
 
 `
+
+export const Input = styled.input`
+
+`
+
+export const SubmitBtn = styled.button`
+
+`
