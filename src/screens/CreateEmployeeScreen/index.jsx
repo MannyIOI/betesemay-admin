9961d288a758be7aa07321fd9f4f8a4b5ab8ac52
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-04 03:06:30 +0300

    feat (CreateEmployee) created screen

---
+++ src/screens/CreateEmployeeScreen/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Container } from "./style";
+
+export const CreateEmployee = () => {
+    return (
+        <Container>
+            Create Employee Screen
+        </Container>
+    )
+}
