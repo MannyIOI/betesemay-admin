7f34136f51c721951ed3c16ac1520b31f90c078e
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-04 03:04:48 +0300

    fix (EmployeeScreen) Changed to function

--- src/screens/EmployeesScreen/index.jsx
+++ src/screens/EmployeesScreen/index.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import { Container } from "./style";
 
-export default class Employee extends Component {
-    render() {
-        return (
-            <Container>
-                Employee
-            </Container>
-        )
-    }
+export const Employee = () => {
+    return (
+        <Container>
+            Employee
+        </Container>
+    )
 }
