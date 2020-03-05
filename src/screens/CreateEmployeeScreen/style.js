c6462f65d051fbf77e30c39a7d900d5a0159261b
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-05 17:24:30 +0300

    fix (employee) changed create employee screen to grid layout

--- src/screens/CreateEmployeeScreen/style.js
+++ src/screens/CreateEmployeeScreen/style.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
+    display: grid;
 `
 
 export const Input = styled.input`
