4decd1181cac787879c42fad92b96d34fdbc8468
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-05 21:20:39 +0300

    fix (items) Items Screen has pagination

--- src/screens/ItemsScreen/index.jsx
+++ src/screens/ItemsScreen/index.jsx
@@ -7,11 +7,14 @@ import { GET_ALL_ITEMS } from "./queries";
 
 const ItemsPage = (props) => {
     const [items, setItems] = useState([]);
+    const [page, setPage] = useState(0)
 
     const setNewItems = async () =>{ 
         try {
+            console.log(page)
             const { data } = await props.client.query({
-                query: GET_ALL_ITEMS
+                query: GET_ALL_ITEMS,
+                variables: { page }
             });
             setItems(data.getAllItems)
         } catch (error) {
@@ -20,14 +23,24 @@ const ItemsPage = (props) => {
     }
 
     useEffect(() =>{ setNewItems() });
+
+    const onNextClicked = async () => {
+        setPage(page+1)
+        // setNewItems()
+    }
+    const onPrevClicked = async () => {
+        setPage(page-1)
+        // setNewItems()
+    }
     
     return (
         <Container>
             { items.map(item => (<Item key = {item.name} item={item}/>)) }
             
             <NavigationContainer> 
-                <button>Previous</button>
-                <button>Next</button>
+                
+                <button onClick={onPrevClicked} disabled={page<=0}>Previous</button>
+                <button onClick={onNextClicked} disabled={items.length<=9}>Next</button>
                 <button onClick={()=>props.history.push({pathname: "/items/create/"})}>Create</button>
             </NavigationContainer>
         </Container>
--- src/screens/ItemsScreen/queries.js
+++ src/screens/ItemsScreen/queries.js
@@ -1,8 +1,8 @@
 import gql from "graphql-tag";
 
 export const GET_ALL_ITEMS = gql`
-    query get_all_items {
-        getAllItems{
+    query get_all_items ($page: Int) {
+        getAllItems(page: $page){
             name
             description
             count
