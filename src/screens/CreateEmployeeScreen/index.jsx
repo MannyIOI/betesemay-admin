744f31d857800309c8bd4be314c0d6a4970cd136
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-05 10:56:26 +0300

    fix (input) employee input binding

--- src/screens/CreateEmployeeScreen/index.jsx
+++ src/screens/CreateEmployeeScreen/index.jsx
@@ -1,26 +1,45 @@
 import React from 'react'
+import { withApollo } from 'react-apollo'
 import { Container, Input, SubmitBtn } from "./style";
+import { useInput } from "../../hooks/inputHooks";
+import { CREATE_EMPLOYEE } from "./queries";
 
-const CreateEmployee = () => {
-    const [email, setEmail] = useState("")
-    const [phone, setPhone] = useState("")
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [jobTitle, setJobTitle] = useState("")
+const CreateEmployee = ({client}) => {
+    const { value: firstName, bind: bindFirstName } = useInput("")
+    const { value: lastName, bind: bindLastName } = useInput("")
+    const { value: email, bind: bindEmail } = useInput("")
+    const { value: phoneNumber, bind: bindPhone } = useInput("")
+    const { value: jobTitle, bind: bindJobTitle } = useInput("")
 
-    
+    const handleSubmit = async () => {
+        try {
+            const {data} = await client.mutate({
+                mutation: CREATE_EMPLOYEE,
+                variables: { 
+                    firstName, 
+                    lastName, 
+                    email, 
+                    phoneNumber, 
+                    jobTitle
+                 }
+            });
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <Container>
             Create Employee Screen
-            {/* "phoneNumber", "firstName", "lastName", "jobTitle" */}
-            <Input placeholder="Email"/>
-            <Input placeholder="Phone Number" />
-            <Input placeholder="Last Name" />
-            <Input placeholder="Job Title" />
-            <SubmitBtn>Submit</SubmitBtn>
+            <Input placeholder="Email" {...bindEmail} />
+            <Input placeholder="Phone Number" {...bindPhone} type="number"/>
+            <Input placeholder="First Name" {...bindFirstName} />
+            <Input placeholder="Last Name" {...bindLastName} />
+            <Input placeholder="Job Title" {...bindJobTitle} />
+            <SubmitBtn onClick={handleSubmit}>Submit</SubmitBtn>
         </Container>
     )
 }
 
-export default CreateEmployee;
+export default withApollo(CreateEmployee);
