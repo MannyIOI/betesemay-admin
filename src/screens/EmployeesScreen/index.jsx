19ef49af6213bad2f7a843e9f4d31510468055e0
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-12 18:44:22 +0300

    feat (employee) Employee list screen

--- src/screens/EmployeesScreen/index.jsx
+++ src/screens/EmployeesScreen/index.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import { Container } from "./style";
+import { MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
 
 const Employee = () => {
     return (
         <Container>
             Employee Screen
+            <MDBTable bordered hover >
+                <MDBTableHead color="blue" textWhite>
+                    <tr>
+                        <th>head</th>
+                        <th>head</th>
+                        <th>head</th>
+                    </tr>
+                </MDBTableHead>
+                <MDBTableBody>
+                    <tr>
+                        <td>data</td>
+                        <td>data</td>
+                        <td>data</td>
+                    </tr>
+                    <tr>
+                        <td>data</td>
+                        <td>data</td>
+                        <td>data</td>
+                    </tr>
+                    
+                </MDBTableBody>
+            </MDBTable>
         </Container>
     )
 }
