ee8d315e803c5a353740f06208eddaee7c84a135
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-19 11:10:26 +0300

    fixed apollo caching

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,8 +1,19 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
 require("dotenv").config()
 console.log(process.env.REACT_APP_SERVER_ADDRESS+'/graphql')
+const defaultOptions = {
+  query: {
+    fetchPolicy: "no-cache"
+  },
+  mutate: {
+    errorPolicy: "all"
+  }
+};
+
 const client = new ApolloClient({
+  defaultOptions,
   uri: process.env.REACT_APP_SERVER_ADDRESS+'/graphql',
+  cache: new InMemoryCache()
 });
 
 export default client;
