083717ed82c2c160fe40399afec1c77689cc38a1
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-19 22:29:04 +0300

    fix (category) fixed multiple page rendering

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import SideBar from "../components/SideBar"
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom"
 
 import EmployeeContainer from "../screens/EmployeesScreen";
 
@@ -13,6 +13,8 @@ import ItemsByCategory from "../screens/ItemsByCategoryScreen";
 import CreateEmployee from "../screens/CreateEmployeeScreen";
 
 import CategoryContainer from "../screens/CategoryScreen";
+import CreateCategory from "../screens/CategoryCreateScreen"
+
 import CollectDispense from "../screens/DispenseCollectScreen";
 
 import { Container } from "./styles"
@@ -38,18 +40,22 @@ const Routes = () => (
         <Router>
             <Container>
                 <SideBar routes = {routes}/>
-                <Route exact path = "/" render={() => <Redirect to="/items"/>} />
-                
-                <Route exact path = "/items" component = { ItemsContainer }/>
-                <Route exact path = "/items/create" component = { CreateItem }/>
-                <Route exact path = "/items/update/:itemId" component = { UpdateItem } />
-                <Route exact path = "/items/:itemId" component = { CollectDispense } />
+                <Switch>
+                    <Route exact path = "/" render={() => <Redirect to="/items"/>} />
+                    
+                    <Route exact path = "/items" component = { ItemsContainer }/>
+                    <Route exact path = "/items/create" component = { CreateItem }/>
+                    <Route exact path = "/items/update/:itemId" component = { UpdateItem } />
+                    <Route exact path = "/items/:itemId" component = { CollectDispense } />
 
-                <Route exact path = "/employees" component = { EmployeeContainer }/>
-                <Route exact path = "/employees/create" component = { CreateEmployee } />
+                    <Route exact path = "/employees" component = { EmployeeContainer }/>
+                    <Route exact path = "/employees/create" component = { CreateEmployee } />
 
-                <Route exact path = "/categories" component = { CategoryContainer } />
-                <Route exact path = "/categories/:categoryId" component = { ItemsByCategory } />
+                    <Route exact path = "/categories" component = { CategoryContainer } />
+                    <Route exact path = "/categories/create" component = { CreateCategory } />
+                    <Route exact path = "/categories/:categoryId" component = { ItemsByCategory } />
+                </Switch>
+                {/* <Route /> */}
             </Container>
         </Router>
     </div>
