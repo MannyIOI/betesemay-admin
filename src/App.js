4ff028ab8303e013f12927e1aa145276bb0b54f8
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-22 18:13:10 +0300

    feat () added react mdb modal for dispense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './App.css';
+// import "mdbreact/dist/css/mdb.css";
+// import "mdbreact/dist/css/style.css";
 import Router from "./routers";
 
 import { ApolloProvider } from "@apollo/react-hooks";
