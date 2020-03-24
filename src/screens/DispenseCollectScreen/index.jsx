5490d8d9f037d8983d5662d5c34c1ea4ff025056
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-24 21:38:10 +0300

    fix (Item) Dispense Collect Screen fixed to have Collect Modal

--- src/screens/DispenseCollectScreen/index.jsx
+++ src/screens/DispenseCollectScreen/index.jsx
@@ -1,21 +1,63 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
 import { Container } from "./style";
 import { MDBTable } from 'mdbreact';
-import DispenseModal from '../../components/DispenseModal'
+import { withApollo } from "react-apollo";
 
-const DispenseCollect = ({ client, history }) => {
+import DispenseModal from '../../components/DispenseModal'
+import { GET_ITEM_DETAIL } from './queries';
+import CollectModal from '../../components/CollectModal';
+
+
+
+const DispenseCollect = ({ client, history, match }) => {
     const [dispenseModalIsOpen,setDispenseModalIsOpen] = useState(false);
+    const [collectModalIsOpen, setCollectModalIsOpen] = useState(false);
+
+    const [item, setItem] = useState({})
     function openDispenseModal() {
         setDispenseModalIsOpen(true);
     }
     function closeDispenseModal(){
         setDispenseModalIsOpen(false);
     }
+
+    function openCollectModal() {
+        setCollectModalIsOpen(true)
+    }
+    function closeCollectModal() {
+        setCollectModalIsOpen(false)
+    }
     // const DispenseCollect = "";
     
     // const GetItemHistory = "";
 
-    // const GetItemDetail = "";
+    // console.log(match.params.itemId)
+    // const GetItemDetail = async () => {
+    //     try {
+    //         const { data } = await client.query({
+    //             query: GET_ITEM_DETAIL,
+    //             variables: { id: match.params.itemId }
+    //         });
+    //         setItem(data.getItem)
+            
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // };
+
+    useEffect(() => {
+        try {
+            client.query({
+                query: GET_ITEM_DETAIL,
+                variables: { id: match.params.itemId }
+            }).then(res=>{
+                setItem(res.data.getItem)
+            });
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }, [client, setItem, match.params.itemId])
 
     return (
         <Container>
@@ -23,12 +65,17 @@ const DispenseCollect = ({ client, history }) => {
             
             <p>Item Detail</p>
             
-            <button onClick={openDispenseModal}>Dispense Item</button>
-            <button>Collect Item</button>
+            {item.state==="IN_STOCK" && <button onClick={openDispenseModal}>Dispense Item</button>}
+            {item.state==="DISPENSED" && <button onClick={openCollectModal}>Collect Item</button>}
 
-            <DispenseModal isOpen={dispenseModalIsOpen} closeModal = {closeDispenseModal}/>
-
+            <p>Item Id - {item.id}</p>
+            <p>Description - {item.descritpion}</p>
+            <p>State - {item.state}</p>
+            <p>Dispense Period - {item.dispense_period}</p>
+            <DispenseModal isOpen={dispenseModalIsOpen} closeModal = {closeDispenseModal} item={item}/>
+            <CollectModal isOpen={collectModalIsOpen} closeModal = {closeCollectModal} item={item}/>
             <p>Item History</p>
+
             <MDBTable>
 
             </MDBTable>
@@ -36,4 +83,4 @@ const DispenseCollect = ({ client, history }) => {
     )
 }
 
-export default DispenseCollect;
+export default withApollo(DispenseCollect);
