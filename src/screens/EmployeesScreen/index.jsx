db3f9f1163527f16bab19b0a7941cd5d09aef384
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-24 10:56:05 +0300

    fix (employees) added styling to employees screen

--- src/screens/EmployeesScreen/index.jsx
+++ src/screens/EmployeesScreen/index.jsx
@@ -1,33 +1,40 @@
 import React from 'react'
-import { Container } from "./style";
+import { Container, TableContainer, ActionContainer } from "./style";
 import { MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
+import 'mdbreact/dist/css/mdb.css';
+import 'mdbreact/dist/css/style.css';
 
-const Employee = () => {
+const Employee = (props) => {
     return (
         <Container>
             Employee Screen
-            <MDBTable bordered hover >
-                <MDBTableHead color="blue" textWhite>
-                    <tr>
-                        <th>head</th>
-                        <th>head</th>
-                        <th>head</th>
-                    </tr>
-                </MDBTableHead>
-                <MDBTableBody>
-                    <tr>
-                        <td>data</td>
-                        <td>data</td>
-                        <td>data</td>
-                    </tr>
-                    <tr>
-                        <td>data</td>
-                        <td>data</td>
-                        <td>data</td>
-                    </tr>
-                    
-                </MDBTableBody>
-            </MDBTable>
+            <TableContainer>
+                <MDBTable bordered hover style={{width: "100%"}}>
+                    <MDBTableHead color="blue" textWhite>
+                        <tr>
+                            <th>head</th>
+                            <th>head</th>
+                            <th>head</th>
+                        </tr>
+                    </MDBTableHead>
+                    <MDBTableBody>
+                        <tr>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><button>Previous</button><button>Next</button></td>
+                            <td></td>
+                        </tr>
+                        
+                    </MDBTableBody>
+                </MDBTable>
+            </TableContainer>
+            <ActionContainer>
+                <button onClick={()=>props.history.push({pathname: "/employees/create/"})}>Create Employee</button>
+            </ActionContainer>
         </Container>
     )
 }
--- src/screens/EmployeesScreen/style.js
+++ src/screens/EmployeesScreen/style.js
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: grid;
+    background: #EAEAEA;
+    grid-template-columns: 1fr 5fr 1fr;
+    grid-template-rows: 1fr 5fr 1fr;
+    width: 90%;
+`
 
+export const TableContainer = styled.div`
+    grid-column: 2/3;
+    grid-row: 2/3;
+`
+export const ActionContainer = styled.div`
+    grid-column: 3/4;
+    grid-row: 3/4;
 `
