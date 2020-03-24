b517c047bb7a30633307536335d1c4d068cf6047
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-24 21:37:30 +0300

    fix (Item) Dispense collect item query added

--- src/components/DispenseModal/queries.js
+++ src/components/DispenseModal/queries.js
@@ -11,6 +11,25 @@ export const GET_ALL_EMPLOYEES = gql`
     }
 `
 
-// export const DISPENSE_ITEM = gql`
+export const DISPENSE_COLLECT_ITEM = gql`
+    mutation dispense_collect_item($id: ID!, $state: String){
+        updateItem(data:{id: $id, state: $state}){
+            state
+        }
+    }
+`
 
-// `
+export const CREATE_ITEM_HISTORY = gql`
+    mutation create_item_history($item: ID!, $to: ID!, 
+                                    $dispense_date: Date!, $expected_return_date: Date!, 
+                                    $return_date: Date){
+                                        
+        createHistory(data:{ dispense_date: $dispense_date,
+                                expected_return_date: $expected_return_date,
+                                return_date: $expected_return_date,
+                                item: $item,
+                                to: $to }){
+        dispense_date
+        }
+    }
+`
