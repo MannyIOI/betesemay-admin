c3d1e5350ec843ce1255ae1f46af220a8fef4490
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-24 21:36:15 +0300

    feat (Item) collect modal Added

---
+++ src/components/CollectModal/index.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import Modal from "react-modal"
+import "react-datepicker/dist/react-datepicker.css";
+import { withApollo } from 'react-apollo';
+import { DISPENSE_COLLECT_ITEM } from '../DispenseModal/queries';
+
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      width                 : '50%',
+      height                : '30%',
+    //   marginRight           : '-50%',
+      padding               : '5%',
+      transform             : 'translate(-50%, -50%)',
+      backdrop              : 'grey'
+    }
+  };
+
+const CollectModal = ({ client, isOpen, closeModal, item }) => {
+    // const [dispenseDate, setDispenseDate] = useState(new Date())
+    // const [expectedReturnDate, setExpectedReturnDate] = useState("")
+    // const [employee, setEmployee] = useState("")
+    // const [modalError, setModalError] = useState("");
+    // const [employees, setEmployees] = useState([])
+
+    Modal.setAppElement('body')
+
+    // function validate(){
+    //     let retVal = false;
+    //     if (employee === "" || employee === null) {
+    //         setModalError("* Employee is not selected")
+    //     }
+    //     else if(dispenseDate === "" || dispenseDate === null){
+    //         setModalError("* Dispense Date not set")
+    //     }
+    //     else if (expectedReturnDate === "" || expectedReturnDate === "") {
+    //         setModalError("* Expected Return date not set")
+    //     }
+    //     else if(expectedReturnDate < dispenseDate) {
+    //         setModalError("* Expected return date can not be before Dispense date")
+    //     }
+    //     else { 
+    //         setModalError("")
+    //         retVal = true 
+    //     }
+    //     return retVal;
+    // }
+
+    const collect = async () => {
+        // if(validate()){
+            let retVal = await Collect()
+            if(retVal) { closeModal(); window.location.reload(false); }
+        // }
+    }
+
+    // useEffect(() => { 
+    //     try {
+    //         client.query({
+    //             query: GET_ALL_EMPLOYEES
+    //         }).then(res => {
+    //             let employees = []
+    //             res.data.getAllEmployees.forEach(employee => {
+    //                 employees.push({label: employee.first_name + " " + employee.last_name + " from " + employee.role,
+    //                                          value: employee.id})
+    //             });
+    //             setEmployees(employees)
+    //         })
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    //  }, [client, setEmployees]);
+    
+    
+    const Collect = async () => {
+        try {
+            await client.mutate({
+                mutation: DISPENSE_COLLECT_ITEM,
+                variables: { id: item.id, state: "IN_STOCK" }
+            })
+            return true
+            
+        } catch (error) {
+            return error
+        }
+    }
+
+    return (
+        <div>
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal">
+                    {/* <Select placeholder="Employees" options={employees} onChange={({value})=>setEmployee(value)}/>
+
+                    <DatePicker placeholderText="Dispense date" 
+                                selected={dispenseDate} 
+                                onChange={date=>setDispenseDate(date)}
+                                style={{width: "100%"}}/>
+
+                    <DatePicker placeholderText="Expected Return date" 
+                                selected={expectedReturnDate} 
+                                onChange={date=>setExpectedReturnDate(date)}
+                                style={{width: "100%"}}/> */}
+                    <p>Are you sure you want to collect this item from `EMPLOYEE`</p>
+                    
+                    {/* <p style={{color:"red"}}>{modalError}</p> */}
+                    <button onClick={collect}>Collect</button>
+                    <button onClick={closeModal}>Cancel</button>
+            </Modal>
+        </div>
+    )
+}
+
+export default withApollo(CollectModal);
---
+++ src/components/CollectModal/queries.js
@@ -0,0 +1,13 @@
+import gql from "graphql-tag"
+
+export const GET_ALL_EMPLOYEES = gql`
+    query get_all_employees{
+        getAllEmployees{
+            id
+            first_name
+            last_name
+            email
+            role
+        }
+    }
+`
