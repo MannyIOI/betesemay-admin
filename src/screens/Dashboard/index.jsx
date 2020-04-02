c256ee3c60fde72e7429367242e90705b24322b5
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-04-02 13:23:12 +0300

    feat (dashboard) created dashboard screen

---
+++ src/screens/Dashboard/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, SearchInput, AccountContainer, ContentContainer, ActivityContainer, CreateContainer } from './styles'
+
+const Dashboard = () => {
+    return (
+        <Container>
+            <SearchInput placeholder="Search Items, Categories, Employees"/>
+            <AccountContainer>
+                Create Account
+            </AccountContainer>
+            <ContentContainer>
+                <CreateContainer>
+                    Create Item, Create Category, Create Employee
+                </CreateContainer>
+            </ContentContainer>
+            <ActivityContainer  >
+                Activity
+            </ActivityContainer>
+        </Container>
+    )
+}
+
+export default Dashboard
