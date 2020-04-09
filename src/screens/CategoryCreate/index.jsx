c892d24f0b8bc49ee9b2fa98aac08a8d1b8b8dcd
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-04-09 16:16:18 +0300

    fix (category) Create button component used and added refetch queries

--- src/screens/CategoryCreate/index.jsx
+++ src/screens/CategoryCreate/index.jsx
@@ -4,10 +4,9 @@ import { Container, FormContainer } from "./style";
 import { useInput } from "../../hooks/inputHooks";
 import { CREATE_CATEGORY } from './queries';
 import { GET_ALL_CATEGORIES } from "../Categories/queries";
-import { CreateButton } from '../Employees/style';
+import CreateButton from '../../components/CreateButton'
 import { Input } from '../CreateEmployee/style';
 import { useState } from 'react';
-import { BeatLoader } from 'react-spinners';
 
 const CreateCategory = ({ client, history }) => {
     const { value: category, bind: bindCategory } = useInput("")
@@ -18,8 +17,7 @@ const CreateCategory = ({ client, history }) => {
         await client.mutate({
             mutation: CREATE_CATEGORY,
             variables: { title: category },
-            refetchQueries: [ { query: GET_ALL_CATEGORIES, variables: { page: 0 } } ],
-            awaitRefetchQueries: true
+            refetchQueries: [ { query: GET_ALL_CATEGORIES, variables: { page: 0 } } ]
         });
 
         history.push({pathname: "/categories"})
@@ -31,14 +29,7 @@ const CreateCategory = ({ client, history }) => {
                 
                 <h2 style={{color: "#6f4685", fontWeight: "700", textAlign: "center"}}>Create Category</h2>
                 <Input placeholder="Category Category" { ...bindCategory }/>
-                <CreateButton onClick={createCategory} disabled ={isLoading} style={isLoading?
-                                                                {border: '3px solid #6f4685', 
-                                                                    background: "#E0E5EC", 
-                                                                    width: '40%'}:
-                                                                {border: '0px'}}>
-
-                    {!isLoading ? 'Create Category' : <BeatLoader color={"#0073cf"} loading={isLoading}/>}
-                </CreateButton>
+                <CreateButton isLoading={isLoading} title="Create Category" onClickHandler={createCategory} />
             </FormContainer>
         </Container>
     )
