7e29a53cb640a5a16bec1c404dd70e9a31b1a906
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-04-10 18:15:53 +0300

    fix (items) added image field to query

--- src/screens/Items/queries.js
+++ src/screens/Items/queries.js
@@ -12,6 +12,7 @@ export const GET_ALL_ITEMS = gql`
                     id
                     title
                 }
+                imageId
             }
             total
         }
--- src/screens/Items/style.js
+++ src/screens/Items/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 export const Container = styled.div`
     height: 100vh;
+    width: 100%;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-template-rows: 1fr 1fr 1fr;
