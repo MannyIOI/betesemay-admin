6bd0f280a113ffa71080d47740e3fff8dcb78350
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-04-10 16:46:20 +0300

    fix (employee profile) removed unnecessary import

--- src/screens/EmployeeProfile/index.jsx
+++ src/screens/EmployeeProfile/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Image } from 'cloudinary-react'
-import { Container, CardContainer, Header, Body, Category, Description, Actions } from './style'
-// import { UpdateButton, DeleteButton } from '../../components/Item/styles'
+import { Container } from './style'
 import Item from '../../components/Item'
-import { FaPen, FaDumpster } from 'react-icons/fa'
 
 export const EmployeeProfile = () => {
     return (
