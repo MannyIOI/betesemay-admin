831700f6a43324b397b63b2c9783bc3cfa82feb2
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-04-13 12:30:20 +0300

    fix (dashboard) time line added for activities

--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Image } from "cloudinary-react";
+import Timeline from 'react-time-line'
 import { Container, 
             SearchInput, 
             AccountContainer, 
@@ -12,21 +13,22 @@ import { Container,
             AccountImage} from './styles'
 import Category from "../../components/Category";
 import { FaArrowLeft, FaArrowRight, FaPlus } from "react-icons/fa";
-import { GET_ALL_CATEGORIES } from './queries';
+import { GET_ALL_CATEGORIES, GET_All_HISTORY } from './queries';
 import { withApollo } from 'react-apollo';
+import Loading from '../../components/Loading';
 
 const Dashboard = ({client, history}) => {
     const [categories, setCategories] = useState([]);
     const [page, setPage] = useState(0);
     const [categoryCount, setCategoryCount] = useState(0);
     const [isLoading, setIsLoading] = useState(true)
+    const [events, setEvents] = useState([])
     
     const setNewCategories = async () =>{ 
         try {
             const { data } = await client.query({
                 query: GET_ALL_CATEGORIES,
                 variables: { page },
-                fetchPolicy: 'catch-and-network',
             })
             setIsLoading(false)
             setCategories(data.getAllCategories.results)
@@ -36,7 +38,34 @@ const Dashboard = ({client, history}) => {
         }
     }
 
-    useEffect   (() =>{ setNewCategories() });
+    const getHistory = async () => {
+        await client.query({
+            query: GET_All_HISTORY,
+            variables: { page: 0 },
+        }).then(res => {
+            let events = []
+            res.data.getAllHistory.results.forEach((element)=>{
+                let text = ""
+                console.log("-")    
+                if(element.type == "COLLECTED"){ text = element.item.title + " collected from " }
+                else { text = element.item.title + " dispensed to " }
+                text += element.to.first_name
+                let date = new Date(parseInt(element.created_at)).toISOString()
+                events.push({ts: date, text: text })
+            })
+            setEvents(events)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    useEffect(() =>{ 
+        setNewCategories();
+     }, [categories]);
+
+    useEffect(() => {
+        getHistory();   
+    }, []);
 
     const onNextClicked = async () => {
         setIsLoading(true)
@@ -77,22 +106,27 @@ const Dashboard = ({client, history}) => {
                     </Create>
                 </CreateContainer>
                 <CategoryContainer>
-                    <ArrowContainer>
+                    <ArrowContainer onClick={onPrevClicked}>
                         <FaArrowLeft size="30" style={{alignSelf: "center", justifySelf: "center"}}/>
                     </ArrowContainer>
                     <div style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr"}}>
+                        { isLoading && <Loading />}
                         { categories.map(category => (<Category key = { category.id } 
                                                     category = { category } 
                                                     refreshCategories = { setNewCategories } 
                                                     history = { history } />)) }
                     </div>
-                    <ArrowContainer>
+                    <ArrowContainer onClick={onNextClicked}>
                         <FaArrowRight size="30" style={{alignSelf: "center", justifySelf: "center"}}/>
                     </ArrowContainer>
                 </CategoryContainer>
+                <div>
+                    Overdue Items
+                </div>
             </ContentContainer>
-            <ActivityContainer  >
-                Activity
+            <ActivityContainer>
+                <p>Activity</p>
+                <Timeline items={events}/>
             </ActivityContainer>
         </Container>
     )
