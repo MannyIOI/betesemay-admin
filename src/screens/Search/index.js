b21926ac0a837a937cba8cf1fe419da243483a5c
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-04-14 21:27:56 +0300

    feat (search) search screen created

---
+++ src/screens/Search/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './style'
+
+const Search = ({  }) => {
+    return (
+        <Container>
+            Search Screen
+        </Container>
+    )
+}
+
+export default Search;
